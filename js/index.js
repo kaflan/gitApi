9a9bda4bd82a7d2a13c0e1e0a4a3f4d98942e593
kaflan <kaf_90@mail.ru>
2015-08-20 15:11:32 +0300

    add temlate dir and temlates

--- js/index.js
+++ js/index.js
@@ -5,20 +5,46 @@
   app.controller('IssuesCtrl', function($scope, listIssues) {
     $scope.data = [];
     listIssues.getBug().success(function(data) {
-      $scope.data =  angular.copy(data);
+      $scope.data = angular.copy(data);
+    }).error(function() {
+      console.log('err');
     });
-
   });
-  //app.directive('myList', function() {});
-
+  //directive
+  app.directive('issues', function() {
+    return {
+      restrict: 'AEC',
+      scope: {
+        items: '=',
+        id: '@'
+      },
+      replace: true,
+      templateURl: '../template/ussues.html'
+    };
+  });
+  app.directive('comments', function() {
+    return {
+      restrict: 'AEC',
+      scope: {
+        items: '=',
+        id: '@'
+      },
+      replace: true,
+      templateURl: '../template/comments.html'
+    };
+  });
   // get issues
-  app.service('listIssues', function($http) {
+  app.service('listIssues', function($http, $location) {
     return {
       getBug: function() {
-        return $http.get('https://api.github.com/repos/Codeception/Codeception/issues');
+        var regExp = /#\/(\w+)\/(\w+)/;
+        var loc = location.hash.match(regExp);
+        var url = $location.path();
+        console.log(loc);
+        console.log(url);
+        return $http.get('https://api.github.com/repos' + url + '/issues');
       }
     };
   });
-
 })();
 //  https://api.github.com/repos/Codeception/Codeception/issues
