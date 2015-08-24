c01073a63b65d8786db2b927f1151fef57e874e3
kaflan <kaf_90@mail.ru>
2015-08-25 00:27:17 +0300

    Lilte refactor

--- js/index.js
+++ js/index.js
@@ -1,64 +1,57 @@
 /* angular Global*/
 (function() {
   'use strict';
-  var list = null;
   var app = angular.module('gitApi', ['ngRoute', 'ngResource']);
-  var number;
-  //#/codeception/codeception/issue/238 
-
-  function saveStorage() {
-    localStorage.setItem('list', JSON.stringify(list));
-  }
-
-  function loadStorage() {
-    list = JSON.parse(localStorage.getItem('list'));
-  }
-  app.controller('ListIssuesCtrl', function($scope, getListIssues, parseUrl, getIssuesInfo) {
-    $scope.org = parseUrl.org;
-    $scope.repo = parseUrl.repo;
-    $scope.number = getIssuesInfo;
-    //load storage
-    if (list !== null) {
-      $scope.data = list;
-    } else {
-      getListIssues.query().then(function(data) {
-        $scope.data = angular.copy(data);
-        console.log($scope.data);
-      });
-    }
-    console.log('I \'m here');
+  app.controller('GetListIssuesCtrl', function($scope,
+    getListIssues) {
+    console.log('work get list');
+    getListIssues.query().then(function(data) {
+      $scope.data = angular.copy(data);
+    });
   });
 
-  // Issuess  ctrl
-  app.controller('IssuesCtrl', function($scope, getListIssues, getIssuesInfo) {
-    $scope.number = Number(getIssuesInfo.number);
-    var number = $scope.number;
-    getListIssues.query().then(function() {
-      var issue = list.filter(function(item) {
-        return item.number === number;
+  app.controller('IssuesCtrl', function($scope, getListIssues, $routeParams) {
+    $scope.number = $routeParams.number;
+    getListIssues.query().then(function(data) {
+      $scope.any = data;
+      var issue = $scope.any.filter(function(item) {
+        return item.number == $scope.number;
       });
       if (!issue.length) return;
       $scope.issue = issue[0];
     });
   });
+
+  // repo and org controller
+  app.controller('SearchRepoCtrl', function($scope, $location) {
+    $scope.searchRepo = function() {
+      $scope.startPage = true;
+      $location.path('/' + $scope.org + '/' + $scope.repo + '/issues');
+    };
+  });
+
   // comments ctrl
   app.controller('CommentsCtrl', function() {
 
   });
-
+  // config
   app.config(['$routeProvider',
     function($routeProvider) {
-      //costile 
       $routeProvider
+        .when('/', {
+          controller: 'SearchRepoCtrl'
+        })
         .when(':/org/:repo/issues', {
-          templateUrl: 'template/nav.html',
-          controller: 'ListIssuesCtrl'
+          templateUrl: 'template/listIssues.html',
+          controller: 'GetListIssuesCtrl'
         })
         .when('/:org/:repo/issues/:number', {
           templateUrl: 'template/issues.html',
           controller: 'IssuesCtrl'
         })
-        .otherwise( {temlpate:'<h1> NO page here<h1>'});
+        .otherwise({
+          temlpate: '<h1> NO page here<h1>'
+        });
     }
   ]);
   // get issues
@@ -66,12 +59,14 @@
     var pathUrl = $location.$$path;
     var regExp = /\/(\w+)\/(\w+)/;
     var loc = pathUrl.match(regExp);
-    var org = loc[1];
-    var repo = loc[2];
-    return {
-      org: org,
-      repo: repo
-    };
+    if (loc !== null) {
+      var org = loc[1];
+      var repo = loc[2];
+      return {
+        org: org,
+        repo: repo
+      };
+    }
 
   });
   app.factory('getListIssues', function($http, parseUrl) {
@@ -79,23 +74,11 @@
     return {
       query: function() {
         return $http.get(url).then(function(res) {
-          list = angular.copy(res.data);
-          // list = $scope.data;
-          saveStorage();
+          var list = angular.copy(res.data);
+          // saveStorage();
           return list;
         });
       }
     };
-  });
-  app.service('getIssuesInfo', function($location) {
-    var pathUrl = $location.$$path;
-    var regExp2 = /([0-9]+)/;
-    var loc = pathUrl.match(regExp2);
-    if (loc !== null) {
-      console.log('issues info number is: ', loc[1]);
-      return {
-        number: loc[1]
-      };
-    }
   });
 })();
