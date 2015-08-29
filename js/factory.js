12d17540a0ebdc5f13183597bd523d2726846a5f
kaflan <kaf_90@mail.ru>
2015-08-30 00:16:24 +0300

    bug fixes

--- js/factory.js
+++ js/factory.js
@@ -12,17 +12,17 @@
           });
         }
       };
+    })
+    // factory get comments list from git hub
+    .factory('getListComments', function($http) {
+      return {
+        query: function(org, repo, number) {
+          var url = 'https://api.github.com/repos/' + org + '/' + repo + '/issues' + '/' + number + '/' + 'comments';
+          return $http.get(url).then(function(res) {
+            var list = angular.copy(res.data);
+            return list;
+          });
+        }
+      };
     });
-  // factory get comments list from git hub
-  angular.module('gitApi').factory('getListComments', function($http) {
-    return {
-      query: function(org, repo, number) {
-        var url = 'https://api.github.com/repos/' + org + '/' + repo + '/issues' + '/' + number + '/' + 'comments';
-        return $http.get(url).then(function(res) {
-          var list = angular.copy(res.data);
-          return list;
-        });
-      }
-    };
-  });
 })();
