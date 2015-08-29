7d8a8d9ac0c523766fdffd9547553db9d1b57bc3
kaflan <kaf_90@mail.ru>
2015-08-29 18:33:30 +0300

    chenhge

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var notify = require("gulp-notify");
+var rename = require("gulp-rename");
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
+var uncss = require('gulp-uncss');
+var wiredep = require('wiredep').stream;
+gulp.task('bower', function() {
+  gulp.src('index.html')
+    .pipe(wiredep({
+      directory: 'bower_components'
+    }))
+    .pipe(gulp.dest('./'));
+});
+gulp.task('concat', function() {
+  gulp.src('css/*.css')
+    .pipe(concat('style.css'))
+    .pipe(autoprefixer('> 1%', 'last 12 versions', 'ie 9'))
+    .pipe(minifyCss(''))
+    .pipe(rename('bundle.min.css'))
+    .pipe(gulp.dest('css/'))
+    .pipe(connect.reload())
+    .pipe(notify('Done!'));
+});
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+gulp.task('watch', function() {
+  gulp.watch('css/*.css');
+});
+gulp.task('default', ['concat', 'connect', 'watch', 'bower']);
